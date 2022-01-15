8fff6daa6a5747a98fda5415436f70a771dfef95
Tim Roman <kan0neego@gmail.com>
2022-01-15 16:20:56 +0700

    Rewrote few lines code

--- src/component/MenuDrop.js
+++ src/component/MenuDrop.js
@@ -2,42 +2,8 @@ import { useState, useEffect, useRef } from "react";
 import "./MenuDrop.css";
 import Icon from "./Icon.js";
 
-/*
-  В родительском компоненте создаём объект типа [link, title]
-  и передаём его в компонент MenuDrop
-*/
-
-const MenuList = prop => {
-  const { list, id } = prop;
-  const targetRef = useRef();
-  useEffect(() => {
-    if (targetRef.current) {
-      const childNodes = targetRef.current.childNodes;
-      let timing = 60;
-      for (let child of childNodes) {
-        child.style.animation = `scaleZ 300ms ${timing}ms ease-in-out forwards`;
-        timing += 60;
-      }
-    }
-  }, [])
-
-  return (
-    <ul className="menu_menuList menu_menuList--disabled" id={id} ref={targetRef}>
-      {
-        list.map((element, index) => {
-          return (
-             <li key={index} id={`dropdown_item-${index}`}>
-              <a href={element.link}>{element.title}</a>
-            </li>
-          )
-        })
-      }
-    </ul>
-  )
-};
-    
 /**
- * This function create a component of button with a drop down list.
+ * This function create a component of button with a dropdown list.
  * @function MenuDrop
  * @param {string} iconID - Identity SVG name within a sprite file.
  * @param {string} titleButton - If a icon of SVG don't define then use title for button.
@@ -71,5 +37,34 @@ const MenuDrop = prop => {
     </div>
   );
 }
+
+const MenuList = prop => {
+  const { list, id } = prop;
+  const targetRef = useRef();
+  useEffect(() => {
+    if (targetRef.current) {
+      const childNodes = targetRef.current.childNodes;
+      let timing = 60;
+      for (let child of childNodes) {
+        child.style.animation = `scaleZ 300ms ${timing}ms ease-in-out forwards`;
+        timing += 60;
+      }
+    }
+  }, [])
+
+  return (
+    <ul className="menu_menuList menu_menuList--disabled" id={id} ref={targetRef}>
+      {
+        list.map((element, index) => {
+          return (
+             <li key={index} id={`dropdown_item-${index}`}>
+              <a href={element.link}>{element.title}</a>
+            </li>
+          )
+        })
+      }
+    </ul>
+  )
+};
 
 export default MenuDrop;
